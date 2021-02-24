6f2f3ced21759cd43f201ec62aed5f2244886477
AnwarHemaimat <Anwarhomaimat@gmail.com>
2021-02-24 05:05:26 -0800

    JavaScript

--- dynamic.js
+++ dynamic.js
@@ -1,15 +1,27 @@
 
 confirm("Please fill your full name");
-    var firstname= prompt("Please provide your first name");
-    var secondname= prompt("Please provide your second name");
+var firstname= prompt("Please provide your first name");
+var secondname= prompt("Please provide your second name");
 console.log(firstname,secondname);
 
-    if (firstname==null || secondname==null || firstname=="" || secondname=="") {
+if (firstname==null || secondname==null || firstname=="" || secondname=="") {
 
-    
-        alert("Please sure if you provide your full name");
-        var firstname= prompt("Please provide your first name");
-        var secondname= prompt("Please provide your second name");
+
+    alert("Please sure if you provide your full name");
+    var firstname= prompt("Please provide your first name");
+    var secondname= prompt("Please provide your second name");
+}
+alert("Welcome"+"  "+firstname+"  "+secondname);
+
+var age;
+function Userage( age){
+    var age = prompt("Can you provide your age please !");
+    if (age < 18){
+        alert("You are a teenager, So welcome in my website");
+    } else {
+        alert("You have my full respect, welcome again");
+        
     }
-    alert("Welcome"+"  "+firstname+"  "+secondname);
+}
 
+Userage(age);
